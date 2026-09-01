81c610c97af03c5caf07fc7ac22afd72464012fb
Renato Golia <renato@golia.tech>
2026-09-01 14:34:28 +0200

    Type provider examples

--- examples/mastodon/hooksmith.config.ts
+++ examples/mastodon/hooksmith.config.ts
@@ -1,12 +1,18 @@
-import type { Config } from "@hooksmith/core";
+import type { Config, Event } from "@hooksmith/core";
 import { postStatus } from "@hooksmith/mastodon";
+
+interface PageData {
+  title: string;
+}
+
+type PageEvent = Event<PageData>;
 
 export default {
   routes: [
     {
       name: "publish-to-mastodon",
       listeners: [
-        postStatus({
+        postStatus<PageEvent>({
           instance: Deno.env.get("MASTODON_INSTANCE")!,
           accessToken: Deno.env.get("MASTODON_ACCESS_TOKEN")!,
           status: (event) => `${event.data.title}\n\n${event.metadata?.url}`,
@@ -15,4 +21,4 @@ export default {
       ],
     },
   ],
-} satisfies Config;
+} satisfies Config<PageEvent>;
